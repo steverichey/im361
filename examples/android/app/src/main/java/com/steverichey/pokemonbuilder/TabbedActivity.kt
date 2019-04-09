a48c0f229b16af56c97479895dc6d96260ca574a
Steve Richey <srichey@gowithfloat.com>
2019-04-08 20:26:39 -0500

    update android example

--- examples/android/app/src/main/java/com/steverichey/pokemonbuilder/TabbedActivity.kt
+++ examples/android/app/src/main/java/com/steverichey/pokemonbuilder/TabbedActivity.kt
@@ -1,2 +1,11 @@
 package com.steverichey.pokemonbuilder
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TabbedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabbed)
+    }
+}
