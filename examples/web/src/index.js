75fea08781f91691839b610cc5cda9afee5abf86
Steve Richey <srichey@gowithfloat.com>
2020-02-17 21:26:31 -0600

    updates for 2/17

--- examples/web/src/index.js
+++ examples/web/src/index.js
@@ -4,7 +4,37 @@ import "./style.css";
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    userName: "Steve",
+    sandwichName: "Hot dog",
+    sandwichOrder: [],
+    sandwichTypes: [
+      "Pizza",
+      "Calzone",
+      "Ravioli",
+      "Ice Cream",
+      "Hot Pocket"
+    ]
+  },
+  computed: {
+    pluralSandwich: function() {
+      if (this.sandwichName == "Ice Cream") {
+        return "Ice Cream Sandwiches";
+      } else if (this.sandwichName == "Ravioli") {
+        return this.sandwichName;
+      } else {
+        return this.sandwichName + "s";
+      }
+    },
+    cannotPlaceOrder: function() {
+      return this.sandwichCount < 1;
+    },
+    isDangerous: function() {
+      return this.sandwichCount >= 10;
+    },
+    sandwichCount: function() {
+      return this.sandwichOrder.length;
+    }
   }
 });
 
