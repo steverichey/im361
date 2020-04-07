d685a2ab64200f2752f3d131f1eb84685e431656
Steve Richey <srichey@gowithfloat.com>
2020-04-07 10:32:00 -0500

    migrate JavaScript example code to Kotlin

--- examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
+++ examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
@@ -2,25 +2,35 @@ package com.steverichey.interplanetarycalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val initialValue = 24
+        val finalValue = fibonacci(initialValue)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = "The fibonacci number at index $initialValue is $finalValue"
     }
 
     // https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
-    function fibonacci(num){
-        var a = 1, b = 0, temp;
+    fun fibonacci(num: Int): Int {
+        var copyOfNum = num
+        var a = 1
+        var b = 0
+        var temp: Int
 
-        while (num >= 0){
-            temp = a;
-            a = a + b;
-            b = temp;
-            num--;
+        while (copyOfNum >= 0) {
+            temp = a
+            a += b
+            b = temp
+            copyOfNum--
         }
 
-        return b;
+        return b
     }
 }
