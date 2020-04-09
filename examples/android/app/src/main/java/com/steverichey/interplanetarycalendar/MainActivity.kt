5c4757d8047bff1ec5e30ccdc58e888fd5444306
Steve Richey <srichey@gowithfloat.com>
2020-04-08 19:14:48 -0500

    wip layouts

--- examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
+++ examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
@@ -10,27 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val initialValue = 24
-        val finalValue = fibonacci(initialValue)
-
-        val textView = findViewById<TextView>(R.id.text_view)
-        textView.text = "The fibonacci number at index $initialValue is $finalValue"
-    }
-
-    // https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
-    fun fibonacci(num: Int): Int {
-        var copyOfNum = num
-        var a = 1
-        var b = 0
-        var temp: Int
-
-        while (copyOfNum >= 0) {
-            temp = a
-            a += b
-            b = temp
-            copyOfNum--
-        }
-
-        return b
+        val textView = findViewById<TextView>(R.id.no_planets_added)
     }
 }
