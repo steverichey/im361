570ff5187a01cce7bffe26323e6d51db7e75aff9
Steve Richey <srichey@gowithfloat.com>
2020-04-20 18:37:53 -0500

    added a fragment

--- examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
+++ examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.steverichey.interplanetarycalendar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
+    lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.no_planets_added)
+        viewPager = findViewById(R.id.view_pager)
+
+        val adapter = MainActivityPagerAdapter(this)
+        viewPager.adapter = adapter
+    }
+
+    private inner class MainActivityPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int {
+            return 3
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return PlanetListFragment()
+        }
     }
 }
