fec83343d053c8e841a9da42a692fe8bd6f216e6
Steve Richey <srichey@gowithfloat.com>
2020-04-27 18:16:41 -0500

    change to app compact activity
    for header

--- examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
+++ examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.steverichey.interplanetarycalendar
 
 import android.graphics.PorterDuff
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -10,7 +11,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity : FragmentActivity() {
+class MainActivity : AppCompatActivity() {
     lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
