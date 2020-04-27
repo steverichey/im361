7f58da48a5f691a58ffa00cba67fae0057cdf318
Steve Richey <srichey@gowithfloat.com>
2020-04-27 18:56:49 -0500

    catch up on in-class changes

---
+++ examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/PlanetItemView.kt
@@ -0,0 +1,22 @@
+package com.steverichey.interplanetarycalendar
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class PlanetItemView : ConstraintLayout {
+    lateinit var textView: TextView
+    lateinit var imageView: ImageView
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        textView = findViewById(R.id.planet_text_view)
+        imageView = findViewById(R.id.planet_image_view)
+    }
+}
--- examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/PlanetListFragment.kt
+++ examples/android/app/src/main/java/com/steverichey/interplanetarycalendar/PlanetListFragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class PlanetListFragment : Fragment() {
+    lateinit var recyclerView: RecyclerView
+    lateinit var viewAdapter: RecyclerView.Adapter<*>
+    lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,6 +36,37 @@ class PlanetListFragment : Fragment() {
             }
 
             dialog.show(fragmentManager!!, "add_planet")
+        }
+
+        val planets = mutableListOf(
+            Planet("Mercury"),
+            Planet("Venus"),
+            Planet("Earth")
+        )
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        viewManager = LinearLayoutManager(context)
+        viewAdapter = PlanetListAdapter(planets)
+    }
+
+    data class Planet(val name: String)
+
+    inner class PlanetListViewHolder(val view: PlanetItemView) : RecyclerView.ViewHolder(view)
+
+    inner class PlanetListAdapter(val data: List<Planet>) : RecyclerView.Adapter<PlanetListViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlanetListViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.view_planet_item, parent, false) as PlanetItemView
+            return PlanetListViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
+
+        override fun onBindViewHolder(holder: PlanetListViewHolder, position: Int) {
+            holder.view.textView.text = data[position].name
+            holder.view.imageView.setImageResource(R.drawable.ic_my_location_black_24dp)
         }
     }
 }
